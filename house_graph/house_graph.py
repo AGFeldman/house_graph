32fb02b421b91e42cbb274139bba50e23d10262f
Aaron Feldman <feldmando@gmail.com>
2014-01-19 00:23:28 -0800

    now processes some options to choose from, like, which houses to consider

--- house_graph/house_graph.py
+++ house_graph/house_graph.py
@@ -1,11 +1,17 @@
 import re
 from urllib2 import urlopen
+from HTMLParser import HTMLParser
+import xml.etree.ElementTree as ET
 
 directory_url = 'http://donut.caltech.edu/directory/'
 
 html = urlopen(directory_url).read()
 option_source = html[html.index('<form'):html.index('</form')]
-option_re = re.compile('<option.*</option>')
-option_list = option_re.findall(option_source)
-for option in option_list:
-    print option
+select_id_re = re.compile('<select id.*?</select>', re.DOTALL)
+category_list = select_id_re.findall(option_source)
+root = ET.fromstring(category_list[0])
+id_ = root.attrib['id']
+option_dic = {}
+for child in root:
+    option_dic[child.text] = child.attrib['value']
+print option_dic
